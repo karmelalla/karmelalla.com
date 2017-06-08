a896dcf786e13c7cf18fbfa59c2a6c76b08cb21f
karmelalla <karmelalla@gmail.com>
2017-06-07 23:59:02 -0700

    fail message

--- assets/main.js
+++ assets/main.js
@@ -6,25 +6,37 @@ $(document).ready(function() {
 
   $subscribeForm.on('submit', function(event){
     event.preventDefault();
-    
+
     $.ajax({
       dataType: 'jsonp',
       url: "https://getsimpleform.com/messages/ajax?form_api_token=25d273b74e476783ccbc1ae150032b7d",
       data: $subscribeForm.serialize(),
     }).done(function() {
-      show_message();
+      done_message();
       $name.val("");
       $email.val("");
+    }).fail(function() {
+      fail_message();
     });
   });
 
-  function show_message() {
+  function done_message() {
     var name = $name.val() ? $name.val() + ", " : "";
 
     swal({
       title: "Cпасибо, " + name + "что подписались на рассылку!",
       text: "Вам придет письмо на " + $email.val() + ", когда выйдет новый пост.",
       timer: 4000,
+      showConfirmButton: true
+    });
+  }
+
+  function fail_message() {
+    swal({
+      title: "Ошибка! Попробуйте подписаться позже.",
+      text: "",
+      type: "error",
+      timer: 3000,
       showConfirmButton: true
     });
   }
