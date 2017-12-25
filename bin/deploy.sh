bbf372cc1e7cf344e8b6cda5d95f39372cb7c6a7
kisakov <isakov90@gmail.com>
2017-12-25 15:39:28 -0800

    fix deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -33,9 +33,6 @@ git add -fA
 git commit --allow-empty -m "$(git log -1 --pretty=%B) [ci skip]"
 git push -f $ORIGIN_URL gh-pages
 
-# Move back to previous branch.
-git checkout -
-
 echo "Deployed Successfully!"
 
 exit 0
